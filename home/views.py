11d7689d0cac0f0f98d364b696bb6c3d61a05d1c
AB0529 <supercrab115@gmail.com>
2020-08-12 02:34:40 -0400

    Added a feature so projects grid is always even

--- home/views.py
+++ home/views.py
@@ -27,6 +27,14 @@ def home(req):
     context['projects'] = [build_project_html(
         repo.language, repo.name, 'No description' if not repo.description else repo.description, f'https://github.com/{repo.full_name}'
     ) for repo in g.get_user().get_repos()]
+
+    # Make sure projects are always even, if not send a blank project card
+    if len(context['projects']) % 2 != 0:
+        # Add blank project card
+        context['projects'].append(
+            build_project_html('python', 'Blank Project', 'Blank project inserted here so projects grid is nice and even :)', '#')
+        )
+
     context['current_year'] = datetime.now().year
 
     return render(req, 'home.html', context)
