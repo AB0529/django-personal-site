ef448e5f8f644e7a9ff045b1cd13415d045e858d
AB0529 <supercrab115@gmail.com>
2020-08-11 20:29:53 -0400

    List comprehention

--- home/views.py
+++ home/views.py
@@ -11,16 +11,14 @@ def build_project_html(language, title, desc, link):
 
     return f'<div class="col s12 m6"> <div class="card blue-grey darken-2"> <div class="card-content white-text"> <span class="card-title"> <i class="devicon-{language.lower()}-plain colored"></i> | <strong>{title}</strong></span> <p>{desc}</p></div><div class="card-action"> <a href="{link}" target="_blank" class="blue-grey darken-4 btn"> <i class="devicon-github-plain colored"></i> <strong>Source</strong></a> </div></div></div>'
 
+
 def home(req):
     """ The home page of the site """
-    g = Github(settings.GH_TOKEN) 
-    projects_html = []
-
+    g = Github(settings.GH_TOKEN)
     # Build all projects into html string
-    for repo in g.get_user().get_repos():
-        projects_html.append(build_project_html(
-                repo.language, repo.name, 'No description' if not repo.description else repo.description , repo.url
-            ))    
+    projects_html = [build_project_html(
+        repo.language, repo.name, 'No description' if not repo.description else repo.description, repo.url
+    ) for repo in g.get_user().get_repos()]
 
     # Send projects html
     context = {}
