70f30b88ee83661641be1387e035d5eb404cd570
AB0529 <supercrab115@gmail.com>
2020-08-12 02:21:24 -0400

    Made project listings better

--- home/views.py
+++ home/views.py
@@ -10,20 +10,23 @@ def build_project_html(language, title, desc, link):
     if not language:
         language = "atom"
 
-    return f'<div class="col s12 m6"> <div class="card small blue-grey darken-2"> <div class="card-content white-text"> <span class="card-title"> <i class="devicon-{language.lower()}-plain colored"></i> | <strong>{title}</strong></span> <p>{desc}</p></div><div class="card-action"> <a href="{link}" target="_blank" class="blue-grey darken-4 btn"> <i class="devicon-github-plain colored"></i> <strong>Source</strong></a> </div></div></div>'
+    return {
+        'lang': language.lower(),
+        'title': title,
+        'desc': desc,
+        'link': link,
+    }
 
 
 def home(req):
     """ The home page of the site """
     g = Github(settings.GH_TOKEN)
-    # Build all projects into html string
-    projects_html = [build_project_html(
+
+    context = {}
+    # Send projects
+    context['projects'] = [build_project_html(
         repo.language, repo.name, 'No description' if not repo.description else repo.description, f'https://github.com/{repo.full_name}'
     ) for repo in g.get_user().get_repos()]
-
-    # Send projects html
-    context = {}
-    context['projects'] = projects_html
     context['current_year'] = datetime.now().year
 
     return render(req, 'home.html', context)
