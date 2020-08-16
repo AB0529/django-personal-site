e9fd27df4868edc31f4cba2ed4b6deacf9c67026
AB0529 <supercrab115@gmail.com>
2020-08-16 08:56:32 +0000

    Polished pics and added delete

--- pics/urls.py
+++ pics/urls.py
@@ -2,5 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
+	path('delete_screenshot/<str:name>', views.delete_screenshot, name='delete_screenshot'),
     path('', views.home, name='pics')
 ]
--- pics/views.py
+++ pics/views.py
@@ -1,19 +1,32 @@
-from django.shortcuts import render
+import requests
 from django.conf import settings
-from requests import get
-from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.http.response import HttpResponseRedirect, HttpResponse
+from django.shortcuts import render
+
 
 def get_screenshots():
 	''' Will get all screenshots in database from API '''
-	req = get(f'https://api.anishb.net/screenshots/all/{settings.API_KEY}')
+	r = requests.get(f'https://api.anishb.net/screenshots/all/{settings.API_KEY}')
 
-	return req.json()["result"]
+	return r.json()["result"]
+
+@login_required
+def delete_screenshot(req, name):
+	r = requests.delete(f'https://api.anishb.net/screenshots/{settings.API_KEY}/{name}')
+	r = r.json()
+
+	if r["state"] == "fail":
+		return HttpResponseRedirect('/')
+
+	return HttpResponseRedirect(req.META.get('HTTP_REFERER'))
 
 @login_required
 def home(req):
 	get_screenshots()
 	context = {}
+	context["delete_url"] = f'{settings.API_HOST}/screenshots/{settings.API_KEY}'
 	context["screenshots"] = [{
 		"name": ss["name"],
 		"img": ss["image"]["Data"],
