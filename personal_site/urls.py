ade8e98ccf1832df57f30d0f28ed2562f4f537bd
AB0529 <supercrab115@gmail.com>
2020-08-16 05:33:01 +0000

    Added pics page

--- personal_site/urls.py
+++ personal_site/urls.py
@@ -20,5 +20,6 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+	path('pics/', include('pics.urls')),
     path('', include('home.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
