d814592aa6fcfb72bb6c86fad830619decab0d36
AB0529 <supercrab115@gmail.com>
2020-08-16 07:44:52 +0000

    Added password auth

--- pics/views.py
+++ pics/views.py
@@ -1,4 +1,24 @@
 from django.shortcuts import render
+from django.conf import settings
+from requests import get
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 
+def get_screenshots():
+	''' Will get all screenshots in database from API '''
+	req = get(f'https://api.anishb.net/screenshots/all/{settings.API_KEY}')
+
+	return req.json()["result"]
+
+@login_required
 def home(req):
-	return render(req, "pics.html")
+	get_screenshots()
+	context = {}
+	context["screenshots"] = [{
+		"name": ss["name"],
+		"img": ss["image"]["Data"],
+		"date": ss["timestamp"]
+	} for ss in get_screenshots()]
+
+
+	return render(req, "pics/pics.html", context)
